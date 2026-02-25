0f44fbf3d4541646ec49d281b93a6e39d1227fa1
ChristopherAGT <christopherchivalan01@gmail.com>
2026-02-25 09:34:05 -0600

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,6 +1,12 @@
 // usuarios.js
 const usuarios = [
   { nombre: "INVITADO", password: "DOMINOS" },
+  { nombre: "Kevin Giovanni Gonzalez Alonzo", password: "dominos" },
+  { nombre: "INVITADO", password: "DOMINOS" },
+  { nombre: "bubu", password: "bubu" },
+  { nombre: "INVITADO", password: "DOMINOS" },
+  { nombre: "bubu", password: "bubu" },
+  { nombre: "INVITADO", password: "DOMINOS" },
   { nombre: "bubu", password: "bubu" },
   { nombre: "Christopher Chivalan", password: "dominos" }
 ];
