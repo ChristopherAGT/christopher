bebe607483389b59c9a6f9bb7ccccc792b76b74a
ChristopherAGT <christopherchivalan01@gmail.com>
2026-02-24 23:28:42 -0600

    Create usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,6 @@
+// usuarios.js
+const usuarios = [
+  { nombre: "Juan Perez", password: "12345" },
+  { nombre: "Maria Lopez", password: "abcde" },
+  { nombre: "Christopher Chivalan", password: "dominos" }
+];
