f57ace0607d481542db47935b09c1c1b772ac735
ChristopherAGT <christopherchivalan01@gmail.com>
2026-03-12 22:40:04 -0600

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -5,7 +5,7 @@ const usuarios = [
   { nombre: "Vincen Miguel Sian Valdez", password: "dominos" },
   { nombre: "Nery Orlando Us Villatoro", password: "dominos" },
   { nombre: "Christopher Alexander Chivalan Tzoy", password: "dominos" },
-  { nombre: "bubu", password: "bubu" },
+  { nombre: "Dulce Pamela Ixtecoc Barrientos", password: "dominos" },
   { nombre: "INVITADO", password: "DOMINOS" },
   { nombre: "bubu", password: "bubu" },
   { nombre: "Christopher Chivalan", password: "dominos" }
